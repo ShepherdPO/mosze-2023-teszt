3f745f6fd47e9d35fe70c53d5640adf9c28a5625
ShepherdPO <145565267+ShepherdPO@users.noreply.github.com>
2023-09-24 10:08:45 +0200

    Hibák keresése

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //N_ELEMENTS félre írva
+    std::cout << '1-100 ertekek duplazasa' //' használata " helyett, nincs sorzárás
+    for (int i = 0;)  //Syntax hiba
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) //végtelen ciklus
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //félkész kiírás
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; //nincs kezdőérték
+    for (int i = 0; i < N_ELEMENTS, i++) //, használata ; helyett
     {
-        atlag += b[i]
+        atlag += b[i] //nincs sorzárás
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
